f43f24e3e1fa336bfac49d3c4ae6add167bbb0ac
root <root@DESKTOP-TKCK3I9.localdomain>
2018-08-15 17:44:12 -0400

    Dynamic buttons based on data

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -12,10 +12,13 @@ export class PlayersComponent implements OnInit {
   constructor(private inmemoryService: InmemoryService) { }
 
   players: Player[]; // List of Players
+  positions: string[]; //List of Positions
+  positions = [];
   selectedPostion: string; // Current position selected
 
   ngOnInit() {
   	this.getPlayers();
+  	this.grabPositions();
   }
 
   onSelect(position: string): void {
@@ -35,6 +38,14 @@ export class PlayersComponent implements OnInit {
 	getPlayers(): void {
 		// Grab the player data asynrchonsouly so it does not impede load time of page
 	  this.inmemoryService.getPlayers().subscribe(players => this.players = players);
+	}
+
+	grabPositions(): void {
+		for (var i = this.players.length - 1; i >= 0; i--) {
+			this.positions.push(this.players[i].position);
+		}
+    // Remove Duplicates from list positions
+    this.positions = this.positions.filter((el, i, a) => i === a.indexOf(el))
 	}
 
 }
